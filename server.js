a396dd8ebb14d8952c4600f0f4d52b40ebcf7a2e
Travis La Marr <exiva@exiva.net>
2013-03-07 04:29:39 -0500

    Pull in image from subreddit. Still needs work. Basic functionality should be there.

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 var http = require('http');
 var url = require("url");
+var request = require('request');
 var bing = require('./lib/bingxml.js');
+var rddt = require('./lib/redditfetcher.js');
 
 http.createServer(function (req, res) {
 	var uri = url.parse(req.url).pathname;
 	console.log(uri);
 	if (uri == "/HPImageArchive.aspx") {
 		res.writeHead(200, {'Content-Type': 'text/xml'});
-		// res.end('Hello World\n');
 		bing.bingxml(function (err, data) {
 			res.end(data);
 		});
 	} else if (uri == "/az/hprichbg/rb/wallpaper_480x800.jpg") {
-		res.writeHead(200, {'Content-Type': 'image/jpeg'});
-		res.end('Hello World\n');
+		rddt.redditmg(function (err,data) {
+			request.get(data).pipe(res)
+		})
 	} else {
 		res.writeHead(404, {'Content-Type': 'text/plain'});
 		res.end('Not found.\n');
 	};
-}).listen(3000);
+}).listen(8008);
